1c2283b015717570a2c9dfdcfc715a25960d337e
liyong <Liyong8027140796>
2020-05-11 22:49:06 +0800

    增加DependOn demo

---
+++ IoC-Container/src/main/java/com/liyong/ioccontainer/service/DependOnService.java
@@ -0,0 +1,14 @@
+package com.liyong.ioccontainer.service;
+
+
+/**
+ * @author <a href="http://youngitman.tech">青年IT男</a>
+ * @version v1.0.0
+ * @className BookService
+ * @description
+ * @JunitTest: {@link  }
+ * @date 2020-05-06 21:38
+ **/
+public class DependOnService {
+
+}
