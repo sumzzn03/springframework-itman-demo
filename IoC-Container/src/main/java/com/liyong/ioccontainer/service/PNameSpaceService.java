dd6521c1fd078f6bbe420c1c336e8b662d984311
liyong <Liyong8027140796>
2020-05-11 22:03:55 +0800

    增加c-namespace 和p-namespace demo

---
+++ IoC-Container/src/main/java/com/liyong/ioccontainer/service/PNameSpaceService.java
@@ -0,0 +1,52 @@
+package com.liyong.ioccontainer.service;
+/**
+ *@author <a href="http://youngitman.tech">青年IT男</a>
+ *@version v1.0.0
+ *@className PNameSpaceService
+ *@description
+ *@JunitTest: {@link  } 
+ *@date 2020-05-11 21:51    
+ *
+ *
+**/
+public class PNameSpaceService {
+
+    private OtherService otherService;
+
+    private String name;
+
+    private String alias;
+
+
+    public OtherService getOtherService() {
+        return otherService;
+    }
+
+    public void setOtherService(OtherService otherService) {
+        this.otherService = otherService;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    @Override
+    public String toString() {
+        return "PNameSpaceService{" +
+                "name='" + name + '\'' +
+                ", alias='" + alias + '\'' +
+                '}';
+    }
+}
